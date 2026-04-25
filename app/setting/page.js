b24aee1700d8ecd1bba9dbc5ddc06ce9749b6aae
gauravkumar2005 <gauravkumar8414pq@gmail.com>
2026-04-25 18:02:00 +0530

    change setting

--- app/setting/page.js
+++ app/setting/page.js
@@ -33,7 +33,7 @@ export default function SettingsPage() {
 
         {/* Notifications */}
         <div className="flex items-center justify-between py-3 border-b">
-          <span>🔔 Notifications</span>
+          <span>🔔 abcd</span>
           <span className="text-gray-400">›</span>
         </div>
 
