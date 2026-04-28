ed90913f89ea9cde51ade7f4f2d4e966f9342723
gauravkumar2005 <gauravkumar8414pq@gmail.com>
2026-04-28 18:46:48 +0530

    update notification page

--- app/notification/page.js
+++ app/notification/page.js
@@ -29,7 +29,7 @@ export default function NotificationsPage() {
       <div className="w-[360px] bg-white rounded-2xl shadow-lg p-5">
         
         {/* Header */}
-        <h2 className="text-lg font-semibold mb-4">Not</h2>
+        <h2 className="text-lg font-semibold mb-4">No</h2>
 
         {/* Notification List */}
         <div className="space-y-4">
