9e4ecddd2d6f1ddb0b3a916a46a7eb7bf6ba00f5
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-05-11 23:33:48 +0530

    make home page responsive

--- app/page.js
+++ app/page.js
@@ -2,10 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="text-4xl m-5 bg-gray-300  text-center p-20">
-     <h2 className=" text-blue-600 font-bold">This is a chatting Website</h2>
-     <div className="bg-red-600 w-50 p-2 text-center  rounded-md">
-      <Link href="/chat">Go to Chat</Link>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300  text-center px-4 py-10">
+     <h2 className=" text-4xl sm:text-5xl md:6xl text-blue-600 font-bold">This is a chatting Website</h2>
+     <br>
+     </br>
+     <div className="bg-red-600 wt-6 p-2 text-center hover:bg-green-600 rounded-md">
+      <Link href="/chat"
+       className="text-white px-6 py-3 rounded-md"
+      >Go to Chat</Link>
      </div>
     </div>
   );
