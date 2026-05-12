a43f25281125e9e938606ffeaaffbee0b0926488
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-05-12 22:58:40 +0530

    add notification page link in settings

--- app/settings/page.js
+++ app/settings/page.js
@@ -43,7 +43,7 @@ export default function SettingsPage() {
 
         {/* Account */}
         <div className="flex items-center justify-between py-3 border-b">
-          <span>👤 </span>
+          <span>👤 Account</span>
           <span className="text-gray-400">›</span>
         </div>
 
