34179569603416efcb831593d5f5c0c809987b00
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-05-13 23:06:16 +0530

    make profile page responsive

--- app/profile/page.js
+++ app/profile/page.js
@@ -25,10 +25,10 @@ export default function ProfilePage() {
           />
 
           {/* Name */}
-          <h2 className="mt-3 text-lg font-semibold">Rohit</h2>
+          <h2 className="mt-3 text-base sm-text-lg font-semibold">Rohit</h2>
 
           {/* Location */}
-          <p className="text-sm text-gray-500">📍 IN, India</p>
+          <p className="text-xs sm:text-sm text-gray-500">📍 IN, India</p>
         </div>
 
         {/* Followers Section */}
