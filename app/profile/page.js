e030a678c48ad4aaefaa76c8029dc8d6a56bcb94
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-05-18 19:37:14 +0530

    make profile page responsive

--- app/profile/page.js
+++ app/profile/page.js
@@ -63,7 +63,7 @@ export default function ProfilePage() {
           {/* Archived */}
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-3">
-              <Heart className="text-gray-600" />
+              <Heart className="text-gray-600 w-5 h-5 sm:w-6 sm:h-6" />
               <div>
                 <Link href="/archived">
                   <p className="font-medium">Archived</p>
